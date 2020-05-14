42f51997d0633e167a066e140219f34dc4a399ff
Philip Lemaster <philiplemaster@yahoo.com>
2020-05-13 20:56:28 -0400

    burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,17 @@
-const orm = require('../config/orm')
+const orm = require('../config/orm');
+
+const burger = {
+    read: function() {
+        orm.selectAll();
+    },
+    
+    post: function() {
+        orm.insertOne();
+    },
+  
+    update: function() {
+      orm.updateOne();
+  }
+};
+
+module.exports = burger;
