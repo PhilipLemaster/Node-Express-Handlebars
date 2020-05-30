29e011168780b2178a4c93fbfe5e86d2d9710c6b
Philip Lemaster <philiplemaster@yahoo.com>
2020-05-30 03:08:12 -0400

    delete button bugfix

--- public/assets/js/jquery.js
+++ public/assets/js/jquery.js
@@ -12,3 +12,18 @@ $(document).ready( function(){
         })
     });
 });
+
+$(document).ready( function(){
+    $(".delete_form").on("submit", function(event){
+        event.preventDefault()
+        var id = $(this).children(".burger_id").val();
+
+        $.ajax({
+            method: "DELETE",
+            url: "/api/burgers/" + id
+        
+        }).then(function(){
+            location.reload()
+        })
+    });
+});
