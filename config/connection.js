257ca8bde6c27b87ccc28b12de594e7615f4b14d
Philip Lemaster <philiplemaster@yahoo.com>
2020-05-30 13:59:17 -0400

    Jaws DB schema

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
-    port: 3306,
-    user: 'fyx3otew0scinpqe	',
-    password: 'xs2z9u1mtksnevkn',
-    database: 'au34h9cchf2c6p98'
-});
+if (process.env.JAWSDB_URL) {
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+    const connection = mysql.createConnection({
+        host: 'vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'fyx3otew0scinpqe',
+        password: 'xs2z9u1mtksnevkn',
+        database: 'au34h9cchf2c6p98'
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
-DROP DATABASE IF EXISTS burger_db;
-CREATE DATABASE burger_db;
-USE burger_db;
+USE au34h9cchf2c6p98;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    createdAt TIMESTAMP NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     UNIQUE (burger_name)
